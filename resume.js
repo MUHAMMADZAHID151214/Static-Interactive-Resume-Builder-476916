b0f1d3f49b986aa9fa5f2ec376ac10e7aaaba5bb
Muhammad Zahid <164073545+MUHAMMADZAHID151214@users.noreply.github.com>
2024-09-12 23:12:17 +0500

    This is static resume which is created by using html css and typescript

--- resume.js
+++ resume.js
@@ -6,7 +6,7 @@ function toggleSection(sectionId) {
         section.style.display = section.style.display === 'none' ? 'block' : 'none';
     }
 }
-// Optionally, you can initialize sections to be visible or hidden
+// Optionally, initialize sections to be visible or hidden
 document.querySelectorAll('#profile-content, #work-content, #education-content').forEach(el => {
-    el.style.display = 'block'; // Set to 'none' if you want sections hidden by default
+    el.style.display = 'block';
 });
